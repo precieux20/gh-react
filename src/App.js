c804f3a54895114f4c421b50ba67d912e6355a44
root <root@DESKTOP-PRECIEUX.>
2025-04-03 14:14:14 +0200

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> test
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
         </a>
       </header>
     </div>
