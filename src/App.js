d25cebf251359ee3c1c6b01f161c52f812867306
root <root@DESKTOP-PRECIEUX.>
2025-04-03 13:39:27 +0200

    Restore default App.js and App.test.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h1> demo-react </h1>
+          Learn React
         </a>
       </header>
     </div>
