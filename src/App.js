0f976fc344ec034e9691625f283e578addeb100f
root <root@DESKTOP-PRECIEUX.>
2025-04-03 13:30:08 +0200

    add workflow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1> demo-react </h1>
         </a>
       </header>
     </div>
